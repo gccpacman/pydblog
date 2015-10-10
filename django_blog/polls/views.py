9969367db3f9b179aa1c4dc84a98e96ea6334706
realhu1989 <huyunyan@live.cn>
2015-10-10 17:07:08 +0800

    add more views

--- django_blog/polls/views.py
+++ django_blog/polls/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import HttpResponse
 
 
 def index(request):
     return HttpResponse("Hello, Django")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
