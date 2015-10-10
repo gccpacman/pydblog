b5c25f9aab43f5d0d5850f48ee92ecd62d820c97
realhu1989 <huyunyan@live.cn>
2015-10-10 16:52:26 +0800

    add was_published_recently in admin

--- django_blog/polls/admin.py
+++ django_blog/polls/admin.py
@@ -13,5 +13,6 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
     inlines = [ChoiceInline]
 
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
 
 admin.site.register(Question, QuestionAdmin)
--- django_blog/polls/models.py
+++ django_blog/polls/models.py
@@ -1,4 +1,7 @@
+from time import timezone
+from datetime import timedelta
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -6,6 +9,13 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - timedelta(days=1)
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
