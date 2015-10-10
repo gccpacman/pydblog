5249d21276505618449d4cd1b9bc25449cbd1999
realhu1989 <huyunyan@live.cn>
2015-10-10 16:15:30 +0800

    django admin inline

--- django_blog/polls/admin.py
+++ django_blog/polls/admin.py
@@ -2,19 +2,16 @@ from django.contrib import admin
 
 from polls.models import Choice, Question
 
-
-# class QuestionAdmin(admin.ModelAdmin):
-#     fields = [
-#         'pub_date',
-#         'question_text',
-#     ]
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']}),
     ]
+    inlines = [ChoiceInline]
 
 
-admin.site.register(Choice)
 admin.site.register(Question, QuestionAdmin)
