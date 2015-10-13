803df9afb885a4f3a0197186013126ddb897eda8
realhu1989 <huyunyan@live.cn>
2015-10-13 17:48:19 +0800

    add namespace and modify template url

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^', include('blog.urls')),
+    url(r'^', include('blog.urls', namespace='blog')),
     url(r'^polls/', include('polls.urls', namespace='polls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
