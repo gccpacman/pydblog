b8fa1a5f97ef562f3b2e0ca34a25ddbe128c6db9
realhu1989 <huyunyan@live.cn>
2015-10-13 13:15:35 +0800

    add bootstrap app in django settings

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # 3rd part app
+    'bootstrap3',
+    # local app
     'polls',
 )
 
