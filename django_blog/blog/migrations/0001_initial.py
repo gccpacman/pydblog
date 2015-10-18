11df639121c534c2057584139eb8a96b5e9f4cb8
realhu1989 <huyunyan@live.cn>
2015-10-18 12:43:49 +0800

    migrate tables

---
+++ django_blog/blog/migrations/0001_initial.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Entry',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=200)),
+                ('body', models.TextField()),
+                ('slug', models.SlugField(unique=True, max_length=200)),
+                ('publish', models.BooleanField(default=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('modified', models.DateTimeField(auto_now=True)),
+            ],
+            options={
+                'verbose_name': 'Blog Entry',
+                'verbose_name_plural': 'Blog Entries',
+                'ordering': ['-created'],
+            },
+        ),
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('slug', models.SlugField(unique=True, max_length=200)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='entry',
+            name='tags',
+            field=models.ManyToManyField(to='blog.Tag'),
+        ),
+    ]
