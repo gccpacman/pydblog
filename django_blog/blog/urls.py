84e2ea19598e4eda2be1a773baac836f6556f2dc
realhu1989 <huyunyan@live.cn>
2015-10-22 15:55:54 +0800

    make post work and change template url

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,4 +4,6 @@ from blog import views
 urlpatterns = [
     url(r'^$', views.BlogIndex.as_view(), name='index'),
     url(r'^about/$', views.BlogAbout.as_view(), name='about'),
+    url(r'^post/(?P<slug>[-\w]+)$', views.BlogDetail.as_view(), name='post'),
+    url(r'^gallery/$', views.BlogGallery.as_view(), name='gallery'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,7 +2,7 @@ from django.views import generic
 from blog import models
 
 
-class BlogAbout(generic.DetailView):
+class BlogAbout(generic.TemplateView):
     template_name = "blog/about.html"
 
 class BlogIndex(generic.ListView):
@@ -13,3 +13,14 @@ class BlogIndex(generic.ListView):
 class BlogDetail(generic.DetailView):
     model = models.Entry
     template_name = "blog/post.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super(BlogDetail, self).get_context_data(**kwargs)
+        #slug = kwargs.get('slug')
+        #context['post'] = models.Entry.objects.get(slug=slug)
+        #import ipdb; ipdb.set_trace()
+        return context
+    
+class BlogGallery(generic.ListView):
+    model = models.Entry
+    template_name = "blog/gallery.html"
