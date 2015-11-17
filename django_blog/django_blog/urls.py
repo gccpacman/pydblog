8061180e3e9c3c6e9cdce1f83655ac494ef79d81
gccpacman <huyunyan@live.cn>
2015-11-17 14:46:08 +0000

    make it a beta because a lot of bugs

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^/', include('blog.urls', namespace='blog')),
+    url(r'^', include('blog.urls', namespace='blog')),
     url(r'^polls/', include('polls.urls', namespace='polls')),
     url(r'^administrator/', include(admin.site.urls)),
     url(r'^markdown/', include("django_markdown.urls")),
