d6b09744e6e66035e8385a8e13ff78665b948fde
gccpacman <huyunyan@live.cn>
2016-02-14 21:52:09 +0800

    url.py

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -3,10 +3,14 @@ from __future__ import absolute_import, unicode_literals
 from django.conf.urls import include, url
 from django.contrib import admin
 
+# from django.conf import settings
+# from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^', include('blog.urls', namespace='blog')),
     url(r'^polls/', include('polls.urls', namespace='polls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^markdown/', include("django_markdown.urls")),
+# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 ]
